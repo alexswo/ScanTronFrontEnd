241c2f8533939a9134d1aa53b5e4d2894d4aac56
Anthony Xue <anthkx@ucla.edu>
2019-05-16 01:19:49 -0700

    not working login view

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Routes } from './routes';
 import * as serviceWorker from './serviceWorker';
+import LoginView from './views/LoginView'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards.min.css";
 
 ReactDOM.render(
     <Router>
-        <Routes />
+      {localStorage.getItem('user') && <Routes />}
+      {!localStorage.getItem('user') && <Route path='/login' component={LoginView} loggingIn='false' />}
     </Router>,
     document.getElementById('root')
 );
